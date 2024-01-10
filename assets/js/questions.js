05617a94ff1e45c0eb0671f84099384bf8c86227
Nat <n3rodriguez2001@gmail.com>
2024-01-10 15:30:22 -0500

    some html changed

---
+++ assets/js/questions.js
@@ -0,0 +1,36 @@
+// Questions, choices, and answers
+
+var quiz = [
+  {
+    title: 'Which keyword is used to declare a variable that cannot be reassigned?',
+    choices: [ 'const', 'var', 'static'],
+    answer: 'const',
+  },
+  {
+    title: 'What does the `===` operator do in JavaScript?',
+    choices: [ 'Assigns a value to a variable', 'Compares two values for equality', 'Checks if a value is less than or equal to another value'],
+    answer: 'Compares two values for equality',
+  },
+  {
+    title: 'Which function is used to print content to the console in JavaScript?',
+    choices: [ 'console.log()', 'print()', 'display()'],
+    answer: 'console.log()',
+  },
+  {
+    title: 'Which symbol is used for comments in HTML?',
+    choices: [ '<!-- -->', '//', '#'],
+    answer: '<!-- -->',
+  },
+  {
+    title: 'What keyword is used to declare a function in JavaScript?',
+    choices: [ 'define', 'post', 'function'],
+    answer: 'function',
+  },
+  {
+    title: 'Which of the following methods is used to access HTML elements using Javascript?',
+    choices: [ 'getElementbyId()', 'getElementbyClassName()', 'both'],
+    answer: 'both',
+  },
+];
+
+export default quiz;
